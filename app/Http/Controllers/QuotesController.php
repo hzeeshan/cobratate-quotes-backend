163078ce158bf5ae2c6a60bbed29f7376ff4a922
Hafiz Riaz <hafizzeeshan619@gmail.com>
2023-10-29 12:09:13 +0100

    create pagination for quotes

--- app/Http/Controllers/QuotesController.php
+++ app/Http/Controllers/QuotesController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Quote;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class QuotesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $quotes = Quote::all();
+        $page = $request->get('page', 1);
+        $pageSize = 10;
+
+        sleep(1);
+        $quotes = Quote::skip(($page - 1) * $pageSize)->take($pageSize)->get();
+
         return $quotes;
     }
 }
